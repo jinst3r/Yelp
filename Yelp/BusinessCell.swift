0611f11a3ae644b5c00e0af2a6070710c4b4da75
jinst3r <choi.jonathanh@gmail.com>
2015-05-15 01:39:36 -0700

    autolayout video walkthrough

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -35,6 +35,15 @@ class BusinessCell: UITableViewCell {
         // Initialization code
         thumbImageView.layer.cornerRadius = 3
         thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        addressLabel.preferredMaxLayoutWidth = addressLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width        
+        addressLabel.preferredMaxLayoutWidth = addressLabel.frame.size.width
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
